6570bfc10f02880df43de07612ce12e4e05017e6
JOB ABE <Joblemi294@gmail.com>
2021-09-16 01:33:52 +0300

    My BarCode Project

---
+++ BarCode/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BarCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBarCode = new System.Windows.Forms.Button();
+            this.textBoxBarCode = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonQRCode = new System.Windows.Forms.Button();
+            this.textBoxQRCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBarCode
+            // 
+            this.buttonBarCode.Location = new System.Drawing.Point(253, 308);
+            this.buttonBarCode.Name = "buttonBarCode";
+            this.buttonBarCode.Size = new System.Drawing.Size(75, 23);
+            this.buttonBarCode.TabIndex = 0;
+            this.buttonBarCode.Text = "BarCode";
+            this.buttonBarCode.UseVisualStyleBackColor = true;
+            this.buttonBarCode.Click += new System.EventHandler(this.buttonBarCode_Click);
+            // 
+            // textBoxBarCode
+            // 
+            this.textBoxBarCode.Location = new System.Drawing.Point(66, 308);
+            this.textBoxBarCode.Name = "textBoxBarCode";
+            this.textBoxBarCode.Size = new System.Drawing.Size(181, 20);
+            this.textBoxBarCode.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 287);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BarCode";
+            // 
+            // buttonQRCode
+            // 
+            this.buttonQRCode.Location = new System.Drawing.Point(253, 339);
+            this.buttonQRCode.Name = "buttonQRCode";
+            this.buttonQRCode.Size = new System.Drawing.Size(75, 23);
+            this.buttonQRCode.TabIndex = 0;
+            this.buttonQRCode.Text = "QRcode";
+            this.buttonQRCode.UseVisualStyleBackColor = true;
+            this.buttonQRCode.Click += new System.EventHandler(this.buttonQRCode_Click);
+            // 
+            // textBoxQRCode
+            // 
+            this.textBoxQRCode.Location = new System.Drawing.Point(66, 339);
+            this.textBoxQRCode.Name = "textBoxQRCode";
+            this.textBoxQRCode.Size = new System.Drawing.Size(181, 20);
+            this.textBoxQRCode.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "QRCode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxQRCode);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonQRCode);
+            this.Controls.Add(this.textBoxBarCode);
+            this.Controls.Add(this.buttonBarCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBarCode;
+        private System.Windows.Forms.TextBox textBoxBarCode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonQRCode;
+        private System.Windows.Forms.TextBox textBoxQRCode;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BarCode/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBarCode_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code128BarcodeDraw barCode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = barCode.Draw(textBoxBarCode.Text,50);
+        }
+
+        private void buttonQRCode_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw qr = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBox1.Image = qr.Draw(textBoxQRCode.Text, 50);
+        }
+    }
+}
